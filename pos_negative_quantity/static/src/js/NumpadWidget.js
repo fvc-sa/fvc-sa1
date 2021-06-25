9cd1f79afb04c6e9faf32bcadbca37e7f8a7ab4a
haithamgailan <73724415+haithamgailan@users.noreply.github.com>
2021-06-26 01:38:38 +0300

    Add files via upload

---
+++ pos_negative_quantity/static/src/js/NumpadWidget.js
@@ -0,0 +1,24 @@
+odoo.define("pos_access_right.NumpadWidget", function (require) {
+    "use strict";
+
+    const Registries = require("point_of_sale.Registries");
+    const NumpadWidget = require("point_of_sale.NumpadWidget");
+
+    const PosNumpadWidget = (NumpadWidget) =>
+        class extends NumpadWidget {
+
+            get hasMinusControlRights() {
+                if (this.env.pos.get_cashier().hasGroupNegativeQty) {
+                    return true;
+                }
+                else if(this.env.pos.get_cashier().employee_with_negative){
+                    return true;
+                }
+                return false;
+            }
+        };
+
+    Registries.Component.extend(NumpadWidget, PosNumpadWidget);
+
+    return NumpadWidget;
+});
