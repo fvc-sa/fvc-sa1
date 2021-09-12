dcb6b06c704828c4fe04fc38fae53447c0358ca1
haithamgailan <73724415+haithamgailan@users.noreply.github.com>
2021-09-12 14:35:45 +0300

    Add files via upload

---
+++ product_profit_report/product_profit_report/__manifest__.py
@@ -0,0 +1,21 @@
+
+{
+    'name': 'Product Profit Report',
+    'version': '14.0.1.0.0',
+    'summary': 'POS Products Profit Report',
+    "author": "RASHAD ALI",
+    "website": "https://api.whatsapp.com/send?phone=00967773200611",
+    'depends': [
+        'sale_management', 'point_of_sale',
+    ],
+    'data': [
+        'wizard/product_profit_report_wizard_view.xml',
+        'views/product_profit_report_pdf_report.xml',
+        'views/product_profit_report_report.xml',
+        'security/ir.model.access.csv',
+
+    ],
+    'installable': True,
+    'auto_install': False,
+    'application': False,
+}
