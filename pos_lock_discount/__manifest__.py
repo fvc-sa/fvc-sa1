bef2866c2f29dc33eacd6dc6d11099bc6f8101c3
haithamgailan <73724415+haithamgailan@users.noreply.github.com>
2021-09-14 22:29:10 +0300

    Add files via upload

---
+++ pos_lock_discount/__manifest__.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': "POS LOCK  DISCOUNT",
+    'summary': """
+        Lock  discount in Point Of Sale""",
+    'description': """
+        This module add features to lock  discount in Point Of Sale using password
+    """,
+    'author': "Rashad Ali",
+    'website': "https://api.whatsapp.com/send/?phone=967773200611",
+    'category': 'Point Of Sale',
+    'license': "LGPL-3",
+    'version': '14.0.0.0',
+    'depends': ['point_of_sale'],
+    'data': [
+        'views/assets.xml',
+        'views/pos_config_view.xml',
+    ],
+
+}
