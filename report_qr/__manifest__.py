c3b72f78e7dae2bbed4ae1c220d77f8aa10e3d7b
haithamgailan <73724415+haithamgailan@users.noreply.github.com>
2021-09-23 16:08:56 +0300

    Add files via upload

---
+++ report_qr/__manifest__.py
@@ -0,0 +1,13 @@
+# Copyright 2019 Creu Blanca
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Web QR Manager",
+    "version": "14.0.1.0.0",
+    "author": "RASHAD ALI",
+    "summary": "Web QR Generator",
+    "website": "https://api.whatsapp.com/send?phone=00967773200611",
+    "depends": ["web"],
+    "data": [],
+    "installable": True,
+}
